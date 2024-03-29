71239e208838535f7c6f67e740a53ddecd12a905
Your Name <you@example.com>
2024-03-29 17:11:17 +0100

    Modification FirstScreen.kt

--- app/src/main/java/com/iagodavit/tikray/screens/FirstScreen.kt
+++ app/src/main/java/com/iagodavit/tikray/screens/FirstScreen.kt
@@ -3,6 +3,8 @@ package com.iagodavit.tikray.screens
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.ColorRes
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,10 +15,13 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.constraintlayout.compose.ConstraintLayout
+import com.iagodavit.tikray.R
+import com.iagodavit.tikray.R.color.tikrayColor1
 import com.iagodavit.tikray.screens.ui.theme.TikrayTheme
 import org.intellij.lang.annotations.JdkConstants
 
@@ -38,16 +43,17 @@ class FirstScreen : ComponentActivity() {
 }
 
 
-
-
-
 @Composable
 fun MainScreen() {
 
-    ConstraintLayout {
-        val (title, logo, subtitle, user, passwd, button, button1 ) = createRefs()
+    ConstraintLayout(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(colorResource(id = tikrayColor1))
+    ) {
+        val (title, logo, subtitle, user, passwd, button, button1) = createRefs()
         val marginTop = createGuidelineFromTop(0.1f)
-        Text(text = "TIKRAY", modifier = Modifier.constrainAs(title){
+        Text(text = "TIKRAY", modifier = Modifier.constrainAs(title) {
 
 
         }, style = TextStyle(color = Color.White))
@@ -55,13 +61,6 @@ fun MainScreen() {
 
     }
 }
-
-
-
-
-
-
-
 
 
 @Preview(showSystemUi = true)
