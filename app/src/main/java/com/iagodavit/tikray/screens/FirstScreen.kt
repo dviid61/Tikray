37991fd8c878a4d4da4879973077a4637417d7f3
Your Name <you@example.com>
2024-03-29 17:00:32 +0100

    Add color Tikray 1 and modifcate the FirstScreen.kt

--- app/src/main/java/com/iagodavit/tikray/screens/FirstScreen.kt
+++ app/src/main/java/com/iagodavit/tikray/screens/FirstScreen.kt
@@ -3,14 +3,22 @@ package com.iagodavit.tikray.screens
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.constraintlayout.compose.ConstraintLayout
 import com.iagodavit.tikray.screens.ui.theme.TikrayTheme
+import org.intellij.lang.annotations.JdkConstants
 
 class FirstScreen : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,25 +30,43 @@ class FirstScreen : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainScreen()
                 }
             }
         }
     }
 }
 
+
+
+
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen() {
+
+    ConstraintLayout {
+        val (title, logo, subtitle, user, passwd, button, button1 ) = createRefs()
+        val marginTop = createGuidelineFromTop(0.1f)
+        Text(text = "TIKRAY", modifier = Modifier.constrainAs(title){
+
+
+        }, style = TextStyle(color = Color.White))
+
+
+    }
 }
 
-@Preview(showBackground = true)
+
+
+
+
+
+
+
+
+@Preview(showSystemUi = true)
 @Composable
-fun GreetingPreview() {
-    TikrayTheme {
-        Greeting("Android")
-    }
+fun Preview() {
+    MainScreen()
+
 }
