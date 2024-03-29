98debb50390d2283e38d1635e7046fb4210e5185
Your Name <you@example.com>
2024-03-29 17:38:55 +0100

    Modification FirstScreen.kt

--- app/src/main/java/com/iagodavit/tikray/screens/FirstScreen.kt
+++ app/src/main/java/com/iagodavit/tikray/screens/FirstScreen.kt
@@ -9,11 +9,16 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
@@ -22,6 +27,7 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import com.iagodavit.tikray.R
@@ -58,6 +64,9 @@ fun MainScreen() {
     ) {
         val (title, logo, subtitle, user, passwd, button, button1) = createRefs()
         val marginTop = createGuidelineFromTop(0.1f)
+        val psswdText by rememberSaveable {
+            mutableStateOf(" ")
+        }
         Text(
             text = "TIKRAY",
             modifier = Modifier.constrainAs(title) {
@@ -72,11 +81,15 @@ fun MainScreen() {
         Image(
             painter = painterResource(id = logo_empresa),
             contentDescription = "logo de la empresa",
-            modifier = Modifier.constrainAs(logo) {
-                start.linkTo(parent.start)
-                end.linkTo(parent.end)
-                top.linkTo(title.bottom)
-            })
+            modifier = Modifier
+                .size(width = 75.dp, height = 75.dp)
+                .constrainAs(logo) {
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                    top.linkTo(title.bottom)
+                }, )
+
+        OutlinedTextField(value = psswdText , onValueChange = text ->  )
 
 
     }
