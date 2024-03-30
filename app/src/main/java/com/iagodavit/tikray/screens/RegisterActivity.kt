ae8d7ed37fa81721c32538e491d14110bfc1bc13
Your Name <you@example.com>
2024-03-30 18:46:58 +0100

    add functions, and add disabled button when the text Fields are Empty

--- app/src/main/java/com/iagodavit/tikray/screens/RegisterActivity.kt
+++ app/src/main/java/com/iagodavit/tikray/screens/RegisterActivity.kt
@@ -92,7 +92,7 @@ fun myApp(
     var surname by remember { mutableStateOf("") }
     var name by remember { mutableStateOf("") }
 
-    var lista = progressBar(password)
+    var lista = progressBar(passwordText)
     var progres = lista[0].toString().toFloat()
     var colorr = lista[1].toString().toInt()
 
